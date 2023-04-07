01172b7f1d7cd48486114a96abc4deb0c7fae91c
Snehil kumar <snehilkumar4c@gmail.com>
2023-04-07 19:00:45 +0530

    styled components using css modules and 1 other ways

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,26 +1,69 @@
 import React, { useState } from 'react';
+// import styled from 'styled-components';
 
 import Button from '../../UI/Button/Button';
-import './CourseInput.css';
+
+import styles from './CourseInput.module.css';
+
+// const FormControl = styled.div`
+  
+//   margin: 0.5rem 0;
+    
+//   & label {
+//     font-weight: bold;
+//     display: block;
+//     margin-bottom: 0.5rem;
+//   }
+  
+//   & input {
+//     display: block;
+//     width: 100%;
+//     border: 1px solid ${props => props.invalid ? 'red' : '#ccc'};
+//     font: inherit;
+//     line-height: 1.5rem;
+//     padding: 0 0.25rem;
+//   }
+  
+//   & input:focus {
+//     outline: none;
+//     // background: #fad0ec;
+//     // border-color: #8b005d;
+//   }
+  
+//   `;
 
 const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
+  const [isValid, setValid] = useState(true);
 
   const goalInputChangeHandler = event => {
+    if(event.target.value.trim().length > 0) {
+      setValid(true);
+    }
     setEnteredValue(event.target.value);
   };
 
   const formSubmitHandler = event => {
     event.preventDefault();
+    if(enteredValue.trim().length === 0) {
+      setValid(false);
+      return;
+    }
     props.onAddGoal(enteredValue);
   };
 
+  
+
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
+      {/* //added props to styled component  */}
+      {/* <FormControl invalid = {!isValid}> */}
+      <div className={`${styles['form-control']} ${!isValid && styles.invalid}`}>
+
         <label>Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
       </div>
+      {/* </FormControl> */}
       <Button type="submit">Add Goal</Button>
     </form>
   );
